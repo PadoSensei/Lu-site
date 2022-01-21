b3d5aa0cd4335bb4e394d36e0dcf2cb469d05ae3
Padraig O'Donoghue <padosensei@pop-os.localdomain>
2022-01-21 19:45:26 +0000

    Workflow test

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ const Index = () => (
       <div className="container mx-auto px-8 lg:flex">
         <div className="text-center lg:text-left lg:w-1/2">
           <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
-            Main title of your landing page
+            Lu's Marketing Site
           </h1>
           <p className="text-xl lg:text-2xl mt-6 font-light">
             Free landing page template to promote your business startup and generate leads for the
@@ -34,7 +34,7 @@ const Index = () => (
     </section>
     <section id="features" className="py-20 lg:pb-40 lg:pt-48">
       <div className="container mx-auto text-center">
-        <h2 className="text-3xl lg:text-5xl font-semibold">Main Features</h2>
+        <h2 className="text-3xl lg:text-5xl font-semibold">Main Services Offered</h2>
         <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
           <div className="flex-1 px-3">
             <Card className="mb-8">
